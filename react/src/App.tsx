cbb9bea1fe934f1d7f6a44e924be5a5d5bfe3818
Kai Yoshida <kaibukuro@gmail.com>
2023-07-10 00:58:09 +0900

    react-router-domで基本ページを実装

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import { useState } from 'react';
 import './App.css';
+import { Route, NavLink } from 'react-router-dom';
+import SlideRoutes from 'react-slide-routes';
+
+const Home = () => <div className="card home">Home</div>;
+const About = () => <div className="card about">About</div>;
+const News = () => <div className="card news">News</div>;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>Hello Vite + React!</p>+{' '}
-      <p className="text-blue-300 bg-red-600">Hello Vite + React!</p>
+      <h1>ようこそ、SPA練習サイトへ</h1>
+
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/news">News</NavLink>
+      </nav>
+
+      <SlideRoutes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/news" element={<News />} />
+      </SlideRoutes>
     </>
   );
 }
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+// import About from './pages/About';
+// import News from './pages/News';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* # ここからRouting設定（適応させたいコンポーネントをBrowserRouterで挟む。） */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* ここまでRouting設定 */}
   </React.StrictMode>,
-)
+);
